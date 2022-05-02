cc6c49e38cf7f88b15c12a2bfe5bf25240caace5
Rafael Martins Apolinário <rafaelma2007@hotmail.com>
2022-05-02 20:07:14 -0300

    02/05

--- public/Js/index.js
+++ public/Js/index.js
@@ -1,3 +1,6 @@
+const myModal = new bootstrap.Modal ("#register-modal");
+
+
 
 document.getElementById("create-form").addEventListener("submit", function(e){
     e.preventDefault();
@@ -5,5 +8,26 @@ document.getElementById("create-form").addEventListener("submit", function(e){
     const email = document.getElementById("email-create-input").value;
     const password=document.getElementById("password-create-input").value;
     
-    console.log(email, password);
-})
+    if(email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
+
+    if(password.length < 4) {
+        alert ("Preencha a senha com no mínimo 4 dígitos.");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transaction: []
+    });
+    
+    myModal.hide();
+    alert ("conta criada com sucesso.");
+});
+
+function saveAccount(data) {
+    console.log(data);
+}
